9eb4e638f0be2fd3fd28d27967a66f309c9dc8a8
Yu Tanaka <YRK00337@nifty.com>
2020-04-21 19:30:36 +0900

    NGサンプル

--- Assets/Scripts/Shot.cs
+++ Assets/Scripts/Shot.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 namespace SolidSample
@@ -8,6 +9,9 @@ namespace SolidSample
     {
         [Tooltip("ショット速度"), SerializeField]
         float speed = 15f;
+        [Tooltip("爆発プレハブ"), SerializeField]
+        GameObject explosionPrefab = null;
+
 
         Rigidbody rb = null;
 
@@ -15,6 +19,16 @@ namespace SolidSample
         {
             rb = GetComponent<Rigidbody>();
             rb.velocity = Vector3.right * speed;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Enemy"))
+            {
+                Instantiate(explosionPrefab, other.transform.position, Quaternion.identity);
+                Destroy(other.gameObject);
+                Destroy(gameObject);
+            }
         }
     }
 }
