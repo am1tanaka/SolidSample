b8a03d5d4a9c3acf7b8360e82a9a433554f18a93
Yu Tanaka <YRK00337@nifty.com>
2020-05-03 17:19:29 +0900

    実装例

---
+++ Assets/Scripts/TwoWayPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SolidSample
+{
+	public class TwoWayPlayer : PlayerDefault
+	{
+		protected override void RightButtonShot()
+		{
+			Instantiate(shotPrefab, transform.position,
+				Quaternion.Euler(0, 0, 15f));
+			Instantiate(shotPrefab, transform.position,
+				Quaternion.Euler(0, 0, -15f));
+		}
+	}
+}
