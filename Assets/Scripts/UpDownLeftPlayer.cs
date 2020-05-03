70261284bfcc8cc14fa70c4667cc5b3b98530244
Yu Tanaka <YRK00337@nifty.com>
2020-05-03 18:35:12 +0900

    上下左ショットを実装

---
+++ Assets/Scripts/UpDownLeftPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SolidSample
+{
+	public class UpDownLeftPlayer : PlayerDefault
+	{
+		protected override void RightButtonShot()
+		{
+			Instantiate(shotPrefab, transform.position,
+				Quaternion.Euler(0, 0, 90f));
+			Instantiate(shotPrefab, transform.position,
+				Quaternion.Euler(0, 0, -90f));
+			Instantiate(shotPrefab, transform.position,
+				Quaternion.Euler(0, 0, 180f));
+		}
+	}
+}
