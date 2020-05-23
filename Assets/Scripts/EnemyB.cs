6c03c943aecd9e9f743ae2b3b29d607daaa75061
Yu Tanaka <YRK00337@nifty.com>
2020-05-23 23:46:31 +0900

    リスコフの解決策を適用した状態

--- Assets/Scripts/EnemyB.cs
+++ Assets/Scripts/EnemyB.cs
@@ -4,19 +4,12 @@ using UnityEngine;
 
 namespace SolidSample
 {
-    public class EnemyB : MonoBehaviour
+    public class EnemyB : Enemy
     {
-        [Tooltip("爆発プレハブ"), SerializeField]
-        GameObject explosionPrefab = null;
         [Tooltip("最高速度"), SerializeField]
         float maxSpeed = 4;
         [Tooltip("1周の秒数"), SerializeField]
         float rollingTime = 2;
-
-        /// <summary>
-        /// ヒットポイント
-        /// </summary>
-        int hp = 4;
 
         Rigidbody rb = null;
         float theta = 0;
@@ -31,16 +24,6 @@ namespace SolidSample
         {
             theta += (Mathf.PI * 2f / rollingTime) * Time.fixedDeltaTime;
             rb.velocity = Vector3.up * Mathf.Cos(theta) * maxSpeed;
-        }
-
-        public void Damage(int damage)
-        {
-            hp -= damage;
-            if (hp <= 0)
-            {
-                Instantiate(explosionPrefab, transform.position, Quaternion.identity);
-                Destroy(gameObject);
-            }
         }
     }
 }
